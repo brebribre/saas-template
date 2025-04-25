e9296878d8e1ed188966c7663d770f17365196ea
brebribre <70563428+brebribre@users.noreply.github.com>
2025-04-25 14:16:08 +0200

    include built in web search tool

--- backend/controller/tools/tools.py
+++ backend/controller/tools/tools.py
@@ -1,5 +1,6 @@
 from typing import Dict, Any, List
 from .weather_tool import WeatherTool
+from agents import WebSearchTool
 
 class Tools:
     """
@@ -7,6 +8,7 @@ class Tools:
     Tool implementations will be defined in separate files and integrated later.
     """
     def __init__(self):
+        # 1. add new tools here
         self.tools = [
             {
                 "type": "web_search_preview",
@@ -31,30 +33,26 @@ class Tools:
                 }
             } 
         ]
+        # 2. map the tool definitions to the tool implementations here
         self.tools_map = {
-            "get_weather": WeatherTool.get_weather
+            "get_weather": WeatherTool.get_weather,
+            "web_search": WebSearchTool()
         }
+        # 3. make a new <tool_name>_tool.py file for each tool implementation. See weather_tool.py for example
     
     @staticmethod
     def get_all_tools_definitions() -> List[Dict[str, Any]]:
         return Tools().tools
-   
-    def call_tool(self, tool_name: str, tool_args) -> Dict[str, Any]:
-        if tool_name in self.tools_map:
-            return self.tools_map[tool_name](**tool_args)
-        else:
-            raise ValueError(f"Tool {tool_name} not found")
-
-    @staticmethod
-    def get_tools_by_names(tool_names: List[str]) -> List[Dict[str, Any]]:
-        """Get tool definitions for the specified tool names"""
-        return [tool for tool in Tools().get_all_tools_definitions() if tool.get("name") in tool_names]
-
 
     @staticmethod
     def get_all_function_tools() -> List[Dict[str, Any]]:
         """Get all function tools. This if for OpenAI Agent SDK"""
         return Tools().tools_map
+
+    @staticmethod
+    def get_tools_by_names(tool_names: List[str]) -> List[Dict[str, Any]]:
+        """Get tool definitions for the specified tool names"""
+        return [tool for tool in Tools().get_all_tools_definitions() if tool.get("name") in tool_names]
 
     @staticmethod
     def get_function_tools_by_names(tool_names: List[str]) -> List[Any]:
@@ -63,3 +61,12 @@ class Tools:
         """
         tools_map = Tools().tools_map
         return [tools_map[name] for name in tool_names if name in tools_map]
+    
+    # Only for Responses API. OpenAI SDK already has a tool calling mechanism built in.
+    def call_tool(self, tool_name: str, tool_args) -> Dict[str, Any]:
+        if tool_name in self.tools_map:
+            return self.tools_map[tool_name](**tool_args)
+        else:
+            raise ValueError(f"Tool {tool_name} not found")
+
+
